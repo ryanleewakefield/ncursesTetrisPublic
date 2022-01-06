cc01d135a5bcd2f50ca5e2f2a6cde769ddc7eb79
Ryan Wakefield <rlwakefield88@gmail.com>
2022-01-06 13:57:05 -0500

    Implemented Square rotation methods

--- classes/Square.hpp
+++ classes/Square.hpp
@@ -11,6 +11,17 @@
 #include "Tetrimino.hpp"
 
 class Square : public Tetrimino{
-
+public:
+    Square(Environment* env, int color, std::vector<unsigned int>& Xs, std::vector<unsigned int>& Ys)
+    : Tetrimino(env, color, Xs, Ys) {}
+    bool rotateLeft() override;
+    bool rotateRight() override;
 };
+/*  Since a Square Tetrimino cannot rotate, it will never
+    "successfully" rotate, so each method should return false
+    when called.
+*/
+bool Square::rotateLeft(){ return false;}
+bool Square::rotateRight(){ return false;}
+
 #endif
